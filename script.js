de3e66097837990cfbe01d05bafbb44b875102c4
HannahtheeCreative <150756772+HannahtheeCreative@users.noreply.github.com>
2024-03-23 20:48:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttonOne = document.getElementById("start");
+
+const buttonTwo = document.getElementById("keep");
+
+const buttonThree = document.getElementById("witness");
+
+let content = document.querySelector("start"); 
+buttonOne.addEventListener("click",() => 
+{ content.style.color= "black";
+});
+
